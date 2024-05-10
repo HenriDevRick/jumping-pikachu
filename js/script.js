abe42d00f6d6fc12f5118783c84264d1af9d93d3
HenriDevRick <henri.limas@gmail.com>
2024-05-09 23:55:22 -0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,33 +1,40 @@
 const pikachu = document.querySelector('.pikachu');
-const rock = document.querySelector('rock');
+const rock = document.querySelector('.rock');
+let jumpCount = 0;
 
-const jump= () => {
+const jump = () => {
+  if (!pikachu.classList.contains('jump')) {
     pikachu.classList.add('jump');
 
     setTimeout(() => {
-        pikachu.classList.remove('jump');
+      pikachu.classList.remove('jump');
     }, 500);
-}
-
-const loop = setInterval(() =>{
-
-    console.log('loop')
 
     const rockPosition = rock.offsetLeft;
-    const pikachuPosition = +window.getComputedStyle(pikachu).bottom.replace('px','');
-
-    console.log(pikachuPosition);
+    const pikachuPosition = +window.getComputedStyle(pikachu).bottom.replace('px', '');
 
     if (rockPosition <= 93 && rockPosition > 0 && pikachuPosition < 80) {
-
-        rock.style.animation = 'none';
-        rock.style.left = `${rockPosition}px`;
-
-        rock.style.animation = 'none';
-        rock.style.bottom = `${pikachuPosition}px`;
-
+      jumpCount++;
+      console.log(`Jump count: ${jumpCount}`);
     }
+  }
+};
 
-},10);
+const loop = setInterval(() => {
+  const rockPosition = rock.offsetLeft;
+  const pikachuPosition = +window.getComputedStyle(pikachu).bottom.replace('px', '');
+
+  if (rockPosition <= 93 && rockPosition > 0 && pikachuPosition < 80) {
+    rock.style.animation = 'none';
+    rock.style.left = `${rockPosition}px`;
+
+    rock.style.animation = 'none';
+    rock.style.bottom = `${pikachuPosition}px`;
+
+    setTimeout(() => {
+      rock.style.animation = 'rock-animation 2s infinite linear'; // Adicione a classe de animação novamente após um pequeno atraso.
+    }, 10);
+  }
+}, 10);
 
 document.addEventListener('keydown', jump);
